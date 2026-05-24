8132b5217ddd4d08372c2091f3d507edc8224682
Tenku01 <herman.prakoso33@gmail.com>
2026-05-24 22:49:23 +0700

    locale

--- resources/views/livewire/sopir/tugas-aktif.blade.php
+++ resources/views/livewire/sopir/tugas-aktif.blade.php
@@ -90,7 +90,7 @@
                                         <div class="flex items-center gap-4 mt-3">
                                             <span class="flex items-center text-xs font-semibold text-gray-500">
                                                 <svg class="w-4 h-4 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                                                {{ \Carbon\Carbon::parse($task->tanggal_mulai)->format('d M') }} - {{ \Carbon\Carbon::parse($task->tanggal_selesai)->format('d M') }}
+                                                {{ \Carbon\Carbon::parse($task->tanggal_mulai)->translatedFormat('d M') }} - {{ \Carbon\Carbon::parse($task->tanggal_selesai)->translatedFormat('d M') }}
                                             </span>
                                             <span class="flex items-center text-xs font-semibold text-gray-500">
                                                 <svg class="w-4 h-4 mr-1 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
@@ -322,7 +322,7 @@
                                                             </h3>
                                                             <p class="text-[10px] font-bold text-gray-400 mt-0.5 uppercase tracking-widest">
                                                                 @php $waktu = \Carbon\Carbon::parse($log->waktu_log ?? $log->created_at ?? now()); @endphp
-                                                                {{ $waktu->format('d M Y - H:i') }} ({{ $waktu->diffForHumans() }})
+                                                                {{ $waktu->translatedFormat('d M Y - H:i') }} ({{ $waktu->diffForHumans() }})
                                                             </p>
                                                         </div>
                                                     </div>
